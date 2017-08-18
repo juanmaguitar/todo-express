237ee942bdbbb2474f15cc50312b26e15503c9a6
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-08-18 10:37:50 +0200

    removed all

--- routes/task/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const removeTask = require('./handlers/removeTask')
-
-router.delete('/:id', removeTask)
-
-module.exports = router
--- routes/tasks/handlers/getTasks.js
+++
@@ -1,8 +0,0 @@
-const Task = require('../../../models/Task')
-
-function getTasks (req, res) {
-  Task.find()
-    .then(tasks => res.render('tasks', { tasks }))
-}
-
-module.exports = getTasks
